9345e74da6ff701e5d75c3a64e657aa9c22e7d93
user <user@linux.kali>
2023-11-01 11:24:36 +0500

    Integration with OneID done

--- one_id/urls.py
+++ one_id/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import OneIDAuthAPIView, OneIDCodeAPIView
+from .views import OneIDAuthAPIView, OneIDCodeAPIView, OneIDLogoutAPIView
 
 urlpatterns = [
     path('login', OneIDAuthAPIView.as_view(), name='oneid_login'),
+    path('logout', OneIDLogoutAPIView.as_view(), name='oneid_logout'),
     path('code', OneIDCodeAPIView.as_view(), name='oneid_code'),
 ]
